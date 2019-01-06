0a579625bd98eefb0d0192e619e73537e328b09b
rabesonw <weslie.rabeson@gmail.com>
2019-01-06 18:31:24 +0100

    fix contact and credits

--- Piscine/resources/views/contact.blade.php
+++ Piscine/resources/views/contact.blade.php
@@ -1,26 +1,31 @@
 @extends('navbars.navbar')
 
 @section('content')
-
-<div class="container">
+<br>
+<div class="container-fluid">
     <div class="row">
         <div class="col-lg-7">
             <h3> Contactez-nous </h3>
             <form method="post">
                 {{  csrf_field()  }}
                 <div class="form-group">
-                    <input type="email" class="form-control" name="mailClient" placeholder="Email *" value="{{ old('mailClient') }}">
+                    <input type="email" class="form-control" name="mail" placeholder="Email *" value="{{ old('mailClient') }}">
                     <!-- @if ($errors->has('mailClient'))
                         <small>  <div class="alert alert-danger" role="alert"> {{ $errors->first('mailClient') }} </div>  </small>
                     @endif -->
                 </div>
                 <div class="form-group">
+                    <input type="text" class="form-control" name="subject" placeholder="Objet...">
+                </div>
+                <div class="form-group">
                     <textarea type="text" class="form-control" name="textMessage" placeholder="Tapez votre message..." cols="40" rows="5"></textarea>
                 </div>
                 <div class="form-group">
-                    <button type="submit" class="btn btn-primary" name="loginClient"> Envoyer </button>
+                    <button type="submit" class="btn btn-primary" name="submitMessage"> Envoyer </button>
                 </div>
             </form>
         </div>
     </div>
 </div>
+
+@endsection
--- Piscine/resources/views/credits.blade.php
+++ Piscine/resources/views/credits.blade.php
@@ -1,9 +1,7 @@
-<!-- Mentions legales -->
-
 @extends('navbars.navbar')
 
 @section('content')
-
+<br>
 <div class="container-fluid">
     <h3>Mentions légales</h3>
     <div class="row">
@@ -12,3 +10,5 @@
         <p>Phasellus vitae tristique justo. Mauris tincidunt nisi id nulla placerat, vitae commodo lacus feugiat. Maecenas facilisis molestie urna in euismod. Nunc varius ex sodales scelerisque convallis. Suspendisse est nisi, rhoncus at sollicitudin sit amet, fringilla vel odio. Nullam viverra euismod gravida. Sed porttitor risus quam, id pulvinar velit pellentesque placerat. In tincidunt tempor ligula, eu tincidunt arcu tincidunt in. Nulla facilisi. Maecenas elementum, nunc venenatis sollicitudin cursus, est justo pellentesque quam, vitae interdum purus metus eu nulla. Integer condimentum varius auctor. Nulla facilisi. Etiam ac suscipit neque. Quisque laoreet egestas ligula nec imperdiet.</p>
     </div>
 </div>
+
+@endsection
