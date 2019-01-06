7d79417eb11d9256dcf53449e1abf7669e0f6301
Unknown <weslie.rabeson@gmail.com>
2019-01-06 11:21:14 +0100

    add credits view to fix

---
+++ Piscine/resources/views/credits.blade.php
@@ -0,0 +1,14 @@
+<!-- Mentions legales -->
+
+@extends('navbars.navbar')
+
+@section('content')
+
+<div class="container-fluid">
+    <h3>Mentions légales</h3>
+    <div class="row">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas laoreet mattis nisi non ultricies. Sed dapibus ipsum nec lorem luctus, id maximus lectus varius. Donec odio dolor, sagittis non gravida non, imperdiet sit amet est. Praesent elementum dolor ac sem fringilla mattis. Aliquam accumsan velit augue. Nullam blandit at dolor in scelerisque. Praesent eu mi justo. Duis eu ultrices mauris, ut vestibulum dui. Cras non ultricies orci, vitae elementum neque.</p>
+        <p>Vivamus scelerisque hendrerit venenatis. Phasellus ut dolor in erat fermentum dapibus in sed sem. Donec efficitur elit non nibh tempus feugiat. Pellentesque at libero in nisl volutpat viverra. In hac habitasse platea dictumst. Morbi eu congue tortor, eget iaculis sapien. Suspendisse pharetra arcu ac tellus ullamcorper malesuada nec ut lectus. In sodales risus nec tempor tempus. Fusce id tortor sapien. Nulla non porttitor dui.</p>
+        <p>Phasellus vitae tristique justo. Mauris tincidunt nisi id nulla placerat, vitae commodo lacus feugiat. Maecenas facilisis molestie urna in euismod. Nunc varius ex sodales scelerisque convallis. Suspendisse est nisi, rhoncus at sollicitudin sit amet, fringilla vel odio. Nullam viverra euismod gravida. Sed porttitor risus quam, id pulvinar velit pellentesque placerat. In tincidunt tempor ligula, eu tincidunt arcu tincidunt in. Nulla facilisi. Maecenas elementum, nunc venenatis sollicitudin cursus, est justo pellentesque quam, vitae interdum purus metus eu nulla. Integer condimentum varius auctor. Nulla facilisi. Etiam ac suscipit neque. Quisque laoreet egestas ligula nec imperdiet.</p>
+    </div>
+</div>
